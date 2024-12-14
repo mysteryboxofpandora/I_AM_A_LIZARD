46ffead80dde05b0e26fb7db5deb4b437b4cf999
mysteryboxofpandora <mysteryboxofpandora@outlook.com>
2024-12-14 16:32:51 +0300

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js"
 
-//y7C0AciDx7vtzdqu      database pass for new project
-const SUPABASE_URL = 'https://eiiyykltshihgfqobmqc.supabase.co'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpaXl5a2x0c2hpaGdmcW9ibXFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM1MDY4NDksImV4cCI6MjA0OTA4Mjg0OX0.X3daVnrgWzbKOSiXwUxAHUbSVFNKKFvS4afZVGTGTlg'
+
+const SUPABASE_URL = ''
+const SUPABASE_KEY = ''
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
